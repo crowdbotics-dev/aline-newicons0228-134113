282a6d1e32f5960e2b29601d406aeda6267d62a9
Crowdbotics <engineering@crowdbotics.com>
2024-02-28 18:17:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aline-newicons0228-134113",
+  url: "https://aline-newicons0228-134113.botics.co",
+  api: "https://aline-newicons0228-134113.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
